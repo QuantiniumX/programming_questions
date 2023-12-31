bdfe20f10e8161484294399c307097f4eeab6950
QuantiniumX <vaibhavvx331@gmail.com>
2023-12-31 16:58:48 +0530

    completed next prime factor with some refactorisation

--- cpp/next_prime_number.cpp
+++ cpp/next_prime_number.cpp
@@ -21,12 +21,17 @@ bool isPrime(u_int64_t n) {
   if (n <= 1) {
     return false;
   }
+  if (n == 2) {
+    return true;
+  }
 
-  for (int i = 2; i <= sqrt(n); ++i) {
+  if (n % 2 == 0) {
+    return false;
+  }
+  for (int i = 3; i <= sqrt(n); i += 2) {
     if (n % i == 0) {
       return false;
     }
   }
-
   return true;
 }
